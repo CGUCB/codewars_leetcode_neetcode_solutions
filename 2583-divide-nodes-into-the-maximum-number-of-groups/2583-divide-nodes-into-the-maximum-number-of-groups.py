0e5f3634ce3da2960aabaa43c25fad2884315e77
Christian Granados <centralsafety23@berkeley.edu>
2025-01-31 15:12:53 -0800

    2583-DIVIDE-NODES-INTO-THE-MAXIMUM-NUMBER-OF-GROUPS Stats: Time: 707 ms (78.44%), Space: 20.3 MB (44.72%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.py
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.py
@@ -2,56 +2,83 @@ from collections import defaultdict, deque
 class Solution:
     def magnificentSets(self, n: int, edges: List[List[int]]) -> int:
 
-        def dfs(node):
-            component_nodes.append(node)
-            visited[node] = True
-            for neighbor in graph[node]:
-                if not visited[neighbor]:
-                    dfs(neighbor)
+        adj = [[] for _ in range(n + 1)]
 
-        def bfs(start_node):
-            maximum_distance = 1
-            distances = [float('inf')] * (n + 1)
-            distances[start_node] = 1
-            queue = deque([start_node])
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
 
-            while queue:
-                current_node = queue.popleft()
-                for neighbor in graph[current_node]:
-                    if distances[neighbor] == float('inf'):
-                        maximum_distance = distances[neighbor] = distances[current_node] + 1
-                        queue.append(neighbor)
+        if not self.is_bipartite(adj,n):
+            return -1
 
-            for node in component_nodes:
-                if distances[node] == float('inf'):
-                    maximum_distance += 1
-                    distances[node] = maximum_distance
+        degree_bfs = [0]*(n + 1)
 
-            for node in component_nodes:
-                for neighbor in graph[node]:
-                    if abs(distances[node] - distances[neighbor]) != 1:
-                        return -1
+        for i in range(1,n+1):
+            degree_bfs[i] = self.bfs_depth(adj,i)
 
-            return maximum_distance
-
-        graph = defaultdict(list)
-        for edge in edges:
-            a, b = edge
-            graph[a].append(b)
-            graph[b].append(a)
-
-        visited = [False] * (n + 1)
-        total_sets = 0
-
-        for i in range(1, n + 1):
+        visited = [0]*(n + 1)
+        groups = 0
+        for i in range(1,n+ 1):
             if not visited[i]:
-                component_nodes = []
-                dfs(i)
-                max_dist = max(bfs(node) for node in component_nodes)
+                groups += self.dfs(adj,visited,degree_bfs,i)
+                
+        return groups 
 
-                if max_dist == -1:
-                    return -1 
+    def dfs(self,adj,visited,degree_bfs,node):
 
-                total_sets += max_dist
+        visited[node] = 1
+        max_depth = degree_bfs[node]
 
-        return total_sets
+        for neigh in adj[node]:
+            if not visited[neigh]:
+                max_depth = max(max_depth , self.dfs(adj,visited,degree_bfs,neigh))
+
+        return max_depth
+
+    def bfs_depth(self,adj,node):
+        
+        queue = deque([(node,1)])
+        visited = [0]*len(adj)
+        visited[node] = 1
+        last_depth = 1
+
+        while queue:
+            cur,depth = queue.popleft()
+            last_depth = depth
+
+            for neigh in adj[cur]:
+                if not visited[neigh]:
+                    visited[neigh] = 1
+                    queue.append((neigh,depth + 1))
+
+        return last_depth
+
+    def is_bipartite(self,adj,n):
+
+        color = [0]*(n + 1)
+
+        for i in range(1,n+1):
+            if color[i] == 0 and not self.bfs_check_bipartite(adj,color,i):
+                return False
+        return True
+
+    def bfs_check_bipartite(self,adj,color,node):
+
+        queue = deque([node])
+        color[node] = 1
+
+        while queue:
+            cur = queue.popleft()
+            if color[cur] == 1:
+                cur_color = 2
+            else:
+                cur_color = 1
+
+            for neigh in adj[cur]:
+                if color[neigh] == 0:
+                    color[neigh] = cur_color
+                    queue.append(neigh)
+                elif color[neigh] != cur_color:
+                    return False
+
+        return True
