e8b2ca758b23238974b07e22f28020c3269c25d7
Christian Granados <centralsafety23@berkeley.edu>
2025-02-07 13:42:51 -0800

    3434-FIND-THE-NUMBER-OF-DISTINCT-COLORS-AMONG-THE-BALLS Stats: Time: 95 ms (27.44%), Space: 63.2 MB (43.26%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
@@ -2,27 +2,22 @@ from collections import defaultdict
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
 
-        b, c, r = defaultdict(lambda : -1), defaultdict(lambda : 0), [0] + [None] * len(queries)
+        b, c, r = defaultdict(lambda : -1), defaultdict(lambda : 0), [None] * len(queries)
 
         for i, (x,y) in enumerate(queries):
-            cur = r[i]
+
             pc = b[x]
-            
-            # If already has color, we need add these changes to c
-            # Then chance in b
+
             if pc != -1:
                 c[pc] -= 1
-                if not c[pc]: # If last color to exist
-                    cur -= 1
+                if not c[pc]:
+                    c.pop(pc)
 
             b[x] = y
             c[y] += 1
-            if c[y] == 1: # If a new color is added
-                cur += 1
-
-            r[i + 1] = cur
+            r[i] = len(c)
         
-        return r[1:]
+        return r
 
                 
             
