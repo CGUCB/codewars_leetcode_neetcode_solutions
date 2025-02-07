c86bca5836861703e02850215a789cc9141486fb
Christian Granados <centralsafety23@berkeley.edu>
2025-02-07 14:20:13 -0800

    1364-TUPLE-WITH-SAME-PRODUCT Stats: Time: 359 ms (63.5%), Space: 46.5 MB (46.62%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,17 +1,16 @@
-from collections import defaultdict
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        d = defaultdict(list)
-        for i, x in enumerate(nums):
-            for y in nums[i + 1:]:
-                if x != y:
-                    d[x * y].append([x, y])
+        d = defaultdict(lambda : 0)
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                p = nums[i] * nums[j]
+                d[p] += 1
         
         total = 0
         for v in d.values():
-            l = len(v)
-            if l >= 2:
-                total += (l * 2) * ((l - 1) * 2)
+            if v >= 2:
+                total += (v * 2) * ((v - 1) * 2)
         
         return total
 
