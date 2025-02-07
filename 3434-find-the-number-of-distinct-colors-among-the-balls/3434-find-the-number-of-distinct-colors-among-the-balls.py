55db225f2587484a07d9f102fa1bb9e724fed3bf
Christian Granados <centralsafety23@berkeley.edu>
2025-02-07 13:40:22 -0800

    3434-FIND-THE-NUMBER-OF-DISTINCT-COLORS-AMONG-THE-BALLS Stats: Time: 107 ms (13.26%), Space: 64.8 MB (22.79%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
@@ -1,31 +1,26 @@
 from collections import defaultdict
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        b, c, r = defaultdict(lambda : -1), defaultdict(lambda : 0), [0]
-        for x, y in queries:
-            cur = r[-1]
-            pc = b[x]
 
-            # If no color, set equal to y and increment c
-            # If a new color, increment cur
-            if pc == -1:
-                b[x] = y
-                c[y] += 1
+        b, c, r = defaultdict(lambda : -1), defaultdict(lambda : 0), [0] + [None] * len(queries)
+
+        for i, (x,y) in enumerate(queries):
+            cur = r[i]
+            pc = b[x]
             
             # If already has color, we need add these changes to c
             # Then chance in b
-            else:
+            if pc != -1:
                 c[pc] -= 1
                 if not c[pc]: # If last color to exist
                     cur -= 1
 
-                c[y] += 1
-                b[x] = y
-            
+            b[x] = y
+            c[y] += 1
             if c[y] == 1: # If a new color is added
                 cur += 1
 
-            r.append(cur)
+            r[i + 1] = cur
         
         return r[1:]
 
