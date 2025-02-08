3a3677e7fa903f79ae8ddfafd87a778e4c443104
Christian Granados <centralsafety23@berkeley.edu>
2025-02-07 19:31:32 -0800

    1801-AVERAGE-TIME-OF-PROCESS-PER-MACHINE Stats: Time: 348 ms (68.68%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -2,24 +2,14 @@
 SELECT
     a1.machine_id,
     ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
-FROM (
-    SELECT 
-        * 
-    FROM 
-        Activity 
-    WHERE 
-        activity_type = 'start'
-    ) AS a1
-JOIN (
-    SELECT 
-        * 
-    FROM 
-        Activity 
-    WHERE 
-        activity_type = 'end'
-    ) AS a2
+FROM 
+    Activity AS a1
+JOIN 
+    Activity AS a2
 ON
     a1.machine_id = a2.machine_id AND
-    a1.process_id = a2.process_id
+    a1.process_id = a2.process_id AND
+    a1.activity_type = 'start' AND 
+    a2.activity_type = 'end'
 GROUP BY
     a1.machine_id
